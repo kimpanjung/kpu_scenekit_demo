6d6a08914486c241136531a1aec18a396736a64c
KimPan Jung <kimpanjung@PJ-iMac-5k-Retina-Display.local>
2015-05-10 23:53:19 +0900

    Initial Commit

---
+++ ZIDLastVersionTests/ZIDLastVersionTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZIDLastVersionTests.swift
+//  ZIDLastVersionTests
+//
+//  Created by KimPan Jung on 2015. 5. 10..
+//  Copyright (c) 2015년 KPJ. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ZIDLastVersionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
